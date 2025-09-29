e572177d0e365be98d478de708f8c8ba0240b6d9
makemake <vukasin@gostovic.me>
2025-09-29 14:19:58 +0200

    feat: add credible e2e boilerplate

--- e2e/src/config/tests-config/environments/local.ts
+++ e2e/src/config/tests-config/environments/local.ts
@@ -11,6 +11,7 @@ const SHOMEI_ENDPOINT = new URL("http://localhost:8998");
 const SHOMEI_FRONTEND_ENDPOINT = new URL("http://localhost:8889");
 const SEQUENCER_ENDPOINT = new URL("http://localhost:8545");
 const TRANSACTION_EXCLUSION_ENDPOINT = new URL("http://localhost:8082");
+const ASSERTION_DA_ENDPOINT = new URL("http://localhost:5001");
 
 const config: Config = {
   L1: {
@@ -52,6 +53,7 @@ const config: Config = {
     shomeiFrontendEndpoint: SHOMEI_FRONTEND_ENDPOINT,
     sequencerEndpoint: SEQUENCER_ENDPOINT,
     transactionExclusionEndpoint: TRANSACTION_EXCLUSION_ENDPOINT,
+    assertionDaEndpoint: ASSERTION_DA_ENDPOINT,
   },
 };
 
