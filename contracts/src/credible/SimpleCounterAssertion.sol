49e39086f17c4cc1f4dc03bcba1734f546cb4b08
makemake <vukasin@gostovic.me>
2025-10-01 13:18:59 +0200

    feat: contract deployments

--- contracts/src/credible/SimpleCounterAssertion.sol
+++ contracts/src/credible/SimpleCounterAssertion.sol
@@ -5,6 +5,7 @@ import {Assertion} from "credible-std/Assertion.sol";
 
 contract Counter {
     uint256 public number;
+    address public owner = address(0x1B9AbEeC3215D8AdE8a33607f2cF0f4F60e5F0D0);
 
     function increment() public {
         number++;
@@ -14,10 +15,15 @@ contract Counter {
 contract SimpleCounterAssertion is Assertion {
     event RunningAssertion(uint256 count);
 
+    Counter public immutable counter;
+
+    constructor(address counterAddress) {
+        require(counterAddress != address(0), "Counter address cannot be zero");
+        counter = Counter(counterAddress);
+    }
+
     function assertCount() public {
-        // FIXME: hardcoded address of the counter contract from above, needs changing
-        // generated with cast compute-address 0x1B9AbEeC3215D8AdE8a33607f2cF0f4F60e5F0D0 --nonce 0
-        uint256 count = Counter(0x729409FAD88CafdA895E41f9ED00Ef4094F8d130).number();
+        uint256 count = counter.number();
         emit RunningAssertion(count);
         if (count > 1) {
             revert("Counter cannot be greater than 1");
