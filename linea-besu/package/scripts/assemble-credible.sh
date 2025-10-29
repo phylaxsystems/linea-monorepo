a65c51cdc86e7b5e6429d044bcb6780df6899c0f
Branimir Malesevic <glbranimir@gmail.com>
2025-10-29 10:46:10 +0100

    credible-version: update credible plugin to 0.1.0-0033afb2 (#20)

--- linea-besu/package/scripts/assemble-credible.sh
+++ linea-besu/package/scripts/assemble-credible.sh
@@ -9,7 +9,7 @@ OWNER="phylaxsystems"
 REPO="credible-layer-besu-plugin"
 GROUP_ID="net.phylax.credible"
 ARTIFACT_ID="credible-layer-besu-plugin"
-VERSION="0.1.0-32b629ce"
+VERSION="0.1.0-0033afb2"
 
 OUTPUT_LOC="./tmp/besu/plugins/$ARTIFACT_ID-$VERSION.jar"
 
