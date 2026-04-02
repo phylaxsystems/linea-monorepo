f2ff300cd0e2f3ec59c19d2ff40e53be56462461
Roman Vaseev <4833306+Filter94@users.noreply.github.com>
2026-04-02 09:58:42 +0200

    Chore: besu update (#2716)
    * Besu update
    
    * Fixed the test for timing out bundles
    
    * Spotless
    
    * Improved the comment
    
    * Another Besu version bump
    
    * Junit platform fix
    
    * Downgrading Besu
    
    * Removed Clique api in docker setup
    
    * Added explicit commons io dependency to the arithmetization
    
    * chore: update besu version in libs.versions.toml after building linea-besu from source
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- linea-besu/plugins/linea-sequencer/sequencer/src/test/java/lineth/sequencer/txselection/LineaTransactionSelectorFactoryTest.java
+++ linea-besu/plugins/linea-sequencer/sequencer/src/test/java/lineth/sequencer/txselection/LineaTransactionSelectorFactoryTest.java
@@ -51,6 +51,7 @@ import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.HardforkId;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.ethereum.core.BlockHeaderBuilder;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.hyperledger.besu.plugin.data.ProcessableBlockHeader;
 import org.hyperledger.besu.plugin.data.TransactionSelectionResult;
@@ -275,6 +276,7 @@ class LineaTransactionSelectorFactoryTest {
   static class FailedTransactionSelectionResultProvider implements ArgumentsProvider {
     @SuppressWarnings("deprecation")
     @Override
+    @SuppressWarnings("deprecation")
     public Stream<? extends Arguments> provideArguments(
         org.junit.jupiter.api.extension.ExtensionContext context) {
       return Stream.of(
