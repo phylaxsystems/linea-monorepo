caf954748de64a6483c4a97252896da2aa2f4144
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-08-10 14:56:42 +0100

    fix(e2e): update plugin reload package name to lineth (#3757)
    Signed-off-by: Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>

--- e2e/src/config/clients/linea-rpc/sequencer-plugins.ts
+++ e2e/src/config/clients/linea-rpc/sequencer-plugins.ts
@@ -1,4 +1,4 @@
 export const SequencerPluginName = {
-  TransactionPoolValidator: "net.consensys.linea.sequencer.txpoolvalidation.LineaTransactionPoolValidatorPlugin",
-  TransactionSelector: "net.consensys.linea.sequencer.txselection.LineaTransactionSelectorPlugin",
+  TransactionPoolValidator: "lineth.sequencer.txpoolvalidation.LineaTransactionPoolValidatorPlugin",
+  TransactionSelector: "lineth.sequencer.txselection.LineaTransactionSelectorPlugin",
 } as const;
